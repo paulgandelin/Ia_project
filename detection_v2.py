7aaf218510602dabc28f5c4f9c8f54a94b208ee6
paulgandelin <131462237+paulgandelin@users.noreply.github.com>
2024-12-04 01:49:05 -0500

    importation

--- detection_v2.py
+++ detection_v2.py
@@ -2,6 +2,13 @@ import cv2
 import time
 from ultralytics import YOLO
 from appel_urgence_v1 import envoi_mail
+#importing Yolo model
+model = YOLO("runs/detect/train24/weights/last.pt")
+
+#importing video stream
+video= cv2.VideoCapture(0)
+
+
 
 
 
