b3b953bda7ec60303db0e9ae4d5a48753ddd3532
OstXm <120989109+OstXm@users.noreply.github.com>
2024-12-04 08:01:20 -0500

    Update trainAI.py

--- trainAI.py
+++ trainAI.py
@@ -1,12 +1,12 @@
 from ultralytics import YOLO
 
 # Charger le modèle pré-entraîné (ex : YOLOv8s)
-model = YOLO("runs/detect/train24/weights/last.pt")
+model = YOLO("runs/detect/train20/weights/last.pt")
 
 # Entraîner le modèle
 model.train(
     data="datasets\data.yaml",  # Chemin vers le fichier data.yaml
-    epochs=5,         # Nombre d'époques
+    epochs=2,         # Nombre d'époques
     batch=16,          # Taille de lot
     imgsz=640          # Taille des images
 )
