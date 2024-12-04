6fdb85bc402427336d92acbfef51b3e75ffb3f3f
paulgandelin <131462237+paulgandelin@users.noreply.github.com>
2024-12-04 01:54:20 -0500

    Update appel_urgence_v1.py

--- appel_urgence_v1.py
+++ appel_urgence_v1.py
@@ -4,4 +4,35 @@ from email.mime.multipart import MIMEMultipart
 from recuperation_bdd import Creation_liste_donnée
 
 
+def envoi_mail(f) : 
+    expediteur, mot_de_passe, destinataire, sujet, corps = Creation_liste_donnée()
+    try:
+        # Configuration du serveur SMTP
+        serveur = smtplib.SMTP('smtp.gmail.com', 587)
+        serveur.starttls()
+
+        # Connexion
+        serveur.login(expediteur, mot_de_passe)
+
+        # Création du message
+        message = MIMEMultipart()
+        message['From'] = expediteur
+        message['To'] = destinataire[0] # changer destinataire plusieurs
+        message['Subject'] = sujet
+
+        # Ajouter le texte du message
+        if f==1: # envoie feu
+            message.attach(MIMEText(corps[0], 'plain'))
+        elif f==0:
+            message.attach(MIMEText(corps[1], 'plain')) #envoie fumée
+        else :
+            print("error")
+            exit(1)
+
+        # Envoi de l'email
+        serveur.sendmail(expediteur, destinataire, message.as_string())
+        serveur.quit()
+
+        print("Email send")
+
 
